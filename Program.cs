559674f3a0dacef7585a28a061449d304fb5b143
azemobaida <47791218+azemobaida@users.noreply.github.com>
2019-02-19 20:20:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("enter number: ");
+			int a = Convert.ToInt32(Console.ReadLine());
+			Console.Write("enter number: ");
+			int b = Convert.ToInt32(Console.ReadLine());
+			Console.Write("enter number: ");
+			int c = Convert.ToInt32(Console.ReadLine());
+
+			if (a > b && a > c)
+			{
+				Console.WriteLine(a + " is the biggest");
+			}
+			else if (b > a && b > c)
+			{
+				Console.WriteLine(b + " is the biggest");
+			}
+			else
+			{
+				Console.WriteLine(c + " is the biggest");
+			}
+		}
+	}
+}
