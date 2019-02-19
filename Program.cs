b7ce01c8c02ac5269d05ebc3574d5680051a9aae
azemobaida <47791218+azemobaida@users.noreply.github.com>
2019-02-19 20:57:48 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,31 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp13
+namespace ConsoleApp15
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			int x;
-			double sum = 0;
-			int i = 0;
-
-			Console.WriteLine("Enter numbers for average (negative to exit):");
-
-			x = Convert.ToInt32(Console.ReadLine());
-
-			while (x >= 0)
+			int x = 1;
+			int a, b, c;
+			do
 			{
-				i++;
-				sum = sum + x;
+				Console.WriteLine(x); // i know x
+				Console.WriteLine("Please give 3 numbers till a * b == c");
+				a = Convert.ToInt32(Console.ReadLine());
+				b = Convert.ToInt32(Console.ReadLine());
+				c = Convert.ToInt32(Console.ReadLine());
 
-				x = Convert.ToInt32(Console.ReadLine());
-
-				
 			}
+			while (a * b != c);
 
-			Console.WriteLine("average = " + sum / i);
+			Console.WriteLine("a =" + a);
+			Console.WriteLine("b =" + b);
+			Console.WriteLine("c =" + c);
+
+
 		}
 	}
 }
